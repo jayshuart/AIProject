84a432590c557fa4011bde8b409a10c146ba8bc1
Joel Shuart <joelshuart@gmail.com>
2017-04-29 15:57:01 -0400

    grid generation

---
+++ Assets/Scripts/InfluenceMaps/GridBuilder.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuilder : MonoBehaviour {
+
+    #region Fields
+    [SerializeField] private GameObject nodePrefab;
+    [SerializeField] private GameObject terrain;
+
+    [SerializeField] private GameObject[,] grid; //array of nodes
+    [SerializeField] private int rows;
+    [SerializeField] private int columns;
+
+    #endregion
+
+    #region Start and Update
+    // Use this for initialization
+    void Start () {
+        //instantiate grid
+        grid = new GameObject[rows, columns];
+
+        //fill grid with nodes
+        BuildGrid();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region Helper Methods
+    private void BuildGrid()
+    {
+        //find spread of nodes
+        float xDiff = terrain.GetComponent<Terrain>().terrainData.size.x / rows;
+        float zDiff = terrain.GetComponent<Terrain>().terrainData.size.z / columns;
+
+        //node scale
+        Vector3 scale = new Vector3(xDiff * 7, zDiff * 7, 1); //its rotated so use z in the y scale spot
+
+        //node position
+        Vector3 pos = new Vector3(0, 150, 0); //get terrains pos
+        pos.x = terrain.transform.position.x + (xDiff / 2); //offset x to start in corner
+        pos.z = terrain.transform.position.z + (zDiff / 2); //offset z
+
+        //fill grid with nodes
+        for (int x = 0; x < rows; x++)
+        {
+            for(int z = 0; z < columns; z++)
+            {
+                //create node and move it to the right spot
+                GameObject node = Instantiate(nodePrefab);
+                node.transform.position = pos; //move to right spot
+                node.transform.localScale = scale; 
+
+                //add node to the matrix
+                grid[x, z] = node;
+
+                //update pos z for next node
+                pos.z += zDiff;
+            }
+
+            //update pos x for next node
+            pos.x += xDiff;
+
+            //reset pos z for next row
+            //pos.z = -terrain.GetComponent<Terrain>().terrainData.size.z / 2;
+            pos.z = terrain.transform.position.z + (zDiff / 2);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/InfluenceMaps/GridNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode : MonoBehaviour {
+
+    #region Fields
+    private SpriteRenderer nodeIcon; 
+    private Dictionary<string, Color32> team; //dictionary of teams colors
+    #endregion
+
+    #region Start and Update
+    // Use this for initialization
+    void Start () {
+        //build dict
+        team = new Dictionary<string, Color32>();
+        team.Add("Red", new Color32(255, 0, 0, 155));
+        team.Add("Green", new Color32(0, 255, 0, 155));
+        team.Add("None", new Color32(101, 101, 101, 155));
+
+        //get spriterenderer and set inital color
+        nodeIcon = this.gameObject.GetComponent<SpriteRenderer>();
+        nodeIcon.color = team["None"];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region Helper Methods
+    /// <summary>
+    /// Defines the color of the node based on the two strengths input
+    /// </summary>
+    /// <param name="redStrength">Influence on this ndoe from red team</param>
+    /// <param name="greenStrength">Influence on this node form green team</param>
+    public void CompareInfluence(int redStrength, int greenStrength)
+    {
+
+    }
+    #endregion
+}
