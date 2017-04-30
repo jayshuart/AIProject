7d9c5afe7145dbfa38c5b9c2c5f0a499d27a3275
Aiden Melendez <itm5305@rit.edu>
2017-04-30 19:03:49 -0400

    Add files via upload
    there are two gamemanagers just so i could get around the two scenes conflicting stuff but it should work anyways.

--- Assets/Scripts/Flocking/GameManager.cs
+++ Assets/Scripts/Flocking/GameManager.cs
@@ -15,6 +15,22 @@ public class GameManager : MonoBehaviour {
     public Camera[] cameras;
     private int currentCameraIndex;
 
+	// Moveable Camera & Units stuff--------------
+	Ray ray;
+	Vector3 hit;
+	private float camSpeed = 200.0f;
+	private float zoomSpeed = 10.0f;
+	private float maxZoom = 42.779f;
+	private float minZoom = 1.0f;
+
+	public GameObject whiteCubePrefab;
+	public GameObject blueCubePrefab;
+	public GameObject yellowCubePrefab;
+	public GameObject blackCubePrefab;
+
+	private int selectedUnit;
+	//----------------------------------
+
     //-----------------------------------------------------------------------
     // Start and Update
     //-----------------------------------------------------------------------
@@ -37,8 +53,13 @@ public class GameManager : MonoBehaviour {
         {
             cameras[0].gameObject.SetActive(true);
             //write to the console which camera is enabled
-            Debug.Log("Camera with name: " + cameras[0].GetComponent<Camera>().name + ", is now enabled");
+			Debug.Log("Camera with name: " + cameras[0].GetComponent<Camera>().name + " (" + currentCameraIndex + "), is now enabled");
         }
+
+		//Units stuff-----------------
+		//default unit to white at first
+		selectedUnit = 1;
+		//----------------------------
     }
 
 
@@ -64,7 +85,7 @@ public class GameManager : MonoBehaviour {
                 //turn off current cam, goto next
                 cameras[currentCameraIndex - 1].gameObject.SetActive(false);
                 cameras[currentCameraIndex].gameObject.SetActive(true);
-                Debug.Log("Camera with name: " + cameras[currentCameraIndex].GetComponent<Camera>().name + ", is now enabled");
+				Debug.Log("Camera with name: " + cameras[currentCameraIndex].GetComponent<Camera>().name + " (" + currentCameraIndex + "), is now enabled");
             }
             else
             {
@@ -72,9 +93,75 @@ public class GameManager : MonoBehaviour {
                 cameras[currentCameraIndex - 1].gameObject.SetActive(false);
                 currentCameraIndex = 0;
                 cameras[currentCameraIndex].gameObject.SetActive(true);
-                Debug.Log("Camera with name: " + cameras[currentCameraIndex].GetComponent<Camera>().name + ", is now enabled");
+				Debug.Log("Camera with name: " + cameras[currentCameraIndex].GetComponent<Camera>().name + " (" + currentCameraIndex + "), is now enabled");
             }
         }
+
+		// Moveable Camera stuff--------------
+		if (currentCameraIndex == 5) {
+			if (Input.GetKey (KeyCode.W)) {
+				cameras [5].transform.position += Vector3.forward * camSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				cameras [5].transform.position += Vector3.left * camSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				cameras [5].transform.position += Vector3.back * camSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				cameras [5].transform.position += Vector3.right * camSpeed * Time.deltaTime;
+			}
+			float scroll = cameras[5].GetComponent<Camera>().fieldOfView;
+			scroll += Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+			scroll = Mathf.Clamp (scroll, minZoom, maxZoom);
+			cameras [5].GetComponent<Camera>().fieldOfView = scroll;
+
+			// Change placeable unit color
+			if (Input.GetKeyDown(KeyCode.Alpha1)) {
+				selectedUnit = 1;
+				Debug.Log ("White Unit selected");
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha2)) {
+				selectedUnit = 2;
+				Debug.Log ("Blue Unit selected");
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha3)) {
+				selectedUnit = 3;
+				Debug.Log ("Yellow Unit selected");
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha4)) {
+				selectedUnit = 4;
+				Debug.Log ("Black Unit selected");
+			}
+
+
+			if (Input.GetMouseButtonDown(0)) {
+				ray = cameras[currentCameraIndex].ScreenPointToRay(Input.mousePosition);
+				RaycastHit rayHit = new RaycastHit ();
+
+				if (Physics.Raycast (ray, out rayHit, Mathf.Infinity)) {
+					//Debug.Log ("Ray hit at:" + rayHit.point);
+					switch(selectedUnit){
+					case 1:
+						Instantiate (whiteCubePrefab, rayHit.point, Quaternion.identity);
+						break;
+					case 2:
+						Instantiate (blueCubePrefab, rayHit.point, Quaternion.identity);
+						break; 
+					case 3:
+						Instantiate (yellowCubePrefab, rayHit.point, Quaternion.identity);
+						break;
+					case 4:
+						Instantiate (blackCubePrefab, rayHit.point, Quaternion.identity);
+						break;
+					}
+
+				}
+				//Debug.DrawLine (ray.origin, rayHit.point, Color.white);
+			}
+		}
+
+		// ------------------------------------
     }
 
     //-----------------------------------------------------------------------
